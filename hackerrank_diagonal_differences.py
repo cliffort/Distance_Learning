ad9466cb93bee2bda396873c6cc8f9e7bf129339
cliffort <59622629+cliffort@users.noreply.github.com>
2020-04-25 20:11:44 -0500

    Add week 2

---
+++ hackerrank_diagonal_differences.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    primary=0
+    secondary=0
+    array_length=len(arr[0])
+    for count in range(array_length):
+        primary+=arr[count][count]
+        secondary+=arr[count][array_length-count-1]
+    return abs(primary-secondary)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
